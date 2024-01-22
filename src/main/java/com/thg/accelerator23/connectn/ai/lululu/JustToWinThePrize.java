136bf27a2af7c3b0eb2db434fdfa594411ee3ca6
Shuhong Lu <uklawson@gmail.com>
2024-01-22 16:07:44 +0000

    modify wrong setup

--- src/main/java/com/thg/accelerator23/connectn/ai/lululu/JustToWinThePrize.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/lululu/JustToWinThePrize.java
@@ -1,14 +1,14 @@
-package com.thg.accelerator23.connectn.ai.yourteam;
+package com.thg.accelerator23.connectn.ai.lululu;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
 
-public class NameAi extends Player {
-  public NameAi(Counter counter) {
+public class JustToWinThePrize extends Player {
+  public JustToWinThePrize(Counter counter) {
     //TODO: fill in your name here
-    super(counter, NameAi.class.getName());
+    super(counter, JustToWinThePrize.class.getName());
   }
 
   @Override
