1d7113d44c1fed7762569f23249723167622da27
Shuhong Lu <uklawson@gmail.com>
2024-01-25 13:15:50 +0000

    change class name

--- src/main/java/com/thg/accelerator23/connectn/ai/lululu/iwanttheconnectfourprize.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/lululu/iwanttheconnectfourprize.java
@@ -1,7 +1,6 @@
 package com.thg.accelerator23.connectn.ai.lululu;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
-import com.thehutgroup.accelerator.connectn.player.BoardStealer;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
@@ -9,10 +8,10 @@ import java.util.ArrayList;
 import java.util.Random;
 
 
-public class JustToWinThePrize extends Player {
-  public JustToWinThePrize(Counter counter) {
+public class iwanttheconnectfourprize extends Player {
+  public iwanttheconnectfourprize(Counter counter) {
     //TODO: fill in your name here
-    super(counter, JustToWinThePrize.class.getName());
+    super(counter, iwanttheconnectfourprize.class.getName());
   }
 
   @Override
@@ -30,14 +29,14 @@ public class JustToWinThePrize extends Player {
     }
 
     //this needs to be adjusted with AI
-    for(int i = 1; i < 8; i ++) {
-      if(gameBoard[i][0].getStringRepresentation().equals(gameBoard[i+1][0].getStringRepresentation())){
-        if(gameBoard[i-1][0] ==null && gameBoard[i+2] == null) {
-          if(i<=5) return i+2;
-          else return i-1;
-        }
-      }
-    }
+//    for(int i = 1; i < 8; i ++) {
+//      if(gameBoard[i][0].getStringRepresentation().equals(gameBoard[i+1][0].getStringRepresentation())){
+//        if(gameBoard[i-1][0] ==null && gameBoard[i+2] == null) {
+//          if(i<=5) return i+2;
+//          else return i-1;
+//        }
+//      }
+//    }
 
 
     Random rand = new Random();
