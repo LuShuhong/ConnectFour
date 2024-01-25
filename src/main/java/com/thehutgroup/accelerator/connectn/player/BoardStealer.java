59cdf9d7a1b79e9e85f41e378fdaea2e419a5fd0
Shuhong Lu <uklawson@gmail.com>
2024-01-25 10:12:55 +0000

    change random behaviour

---
+++ src/main/java/com/thehutgroup/accelerator/connectn/player/BoardStealer.java
@@ -0,0 +1,7 @@
+package com.thehutgroup.accelerator.connectn.player;
+
+public class BoardStealer {
+    public static Counter[][] getBoard(Board board){
+        return board.getCounterPlacements();
+    }
+}
--- src/main/java/com/thg/accelerator23/connectn/ai/lululu/JustToWinThePrize.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/lululu/JustToWinThePrize.java
@@ -1,8 +1,12 @@
 package com.thg.accelerator23.connectn.ai.lululu;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
+import com.thehutgroup.accelerator.connectn.player.BoardStealer;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 
 public class JustToWinThePrize extends Player {
@@ -15,7 +19,19 @@ public class JustToWinThePrize extends Player {
   public int makeMove(Board board) {
     //TODO: some crazy analysis
     //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
+    Counter counter = this.getCounter();
+    Counter[][] gameBoard = BoardStealer.getBoard(board);
 
-    return 5;
+    ArrayList<Integer> availableColumn = new ArrayList<>();
+    for(int i=0; i<10;i++){
+      if(gameBoard[i][7] == null ){
+        availableColumn.add(i);
+      }
+    }
+
+    Random rand = new Random();
+    int randomElement = availableColumn.get(rand.nextInt(availableColumn.size()));
+
+    return randomElement;
   }
 }
