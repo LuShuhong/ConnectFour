d07c0ecd59cfac6e926139addcf6e004dd671cad
Shuhong Lu <uklawson@gmail.com>
2024-01-26 09:28:05 +0000

    seems to be working? but needs to work on random move fallback

--- src/main/java/com/thg/accelerator23/connectn/ai/lululu/iwanttheconnectfourprize.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/lululu/iwanttheconnectfourprize.java
@@ -1,9 +1,6 @@
 package com.thg.accelerator23.connectn.ai.lululu;
 
-import com.thehutgroup.accelerator.connectn.player.Board;
-import com.thehutgroup.accelerator.connectn.player.Counter;
-import com.thehutgroup.accelerator.connectn.player.InvalidMoveException;
-import com.thehutgroup.accelerator.connectn.player.Player;
+import com.thehutgroup.accelerator.connectn.player.*;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -34,16 +31,16 @@ public class iwanttheconnectfourprize extends Player {
 
     //this needs to be adjusted with AI
     //if two columns with the same tokens, block or continue from it
-    for(int i = 1; i < 8; i ++) {
-      if(gameBoard[i][0] != null && gameBoard[i+1][0] != null) {
-        if(gameBoard[i][0].getStringRepresentation().equals(gameBoard[i+1][0].getStringRepresentation())){
-          if(gameBoard[i-1][0] ==null && gameBoard[i+2][0] == null) {
-            if(i<=4) return i+2;
-            else return i-1;
-          }
-        }
-      }
-    }
+//    for(int i = 1; i < 8; i ++) {
+//      if(gameBoard[i][0] != null && gameBoard[i+1][0] != null) {
+//        if(gameBoard[i][0].getStringRepresentation().equals(gameBoard[i+1][0].getStringRepresentation())){
+//          if(gameBoard[i-1][0] ==null && gameBoard[i+2][0] == null) {
+//            if(i<=4) return i+2;
+//            else return i-1;
+//          }
+//        }
+//      }
+//    }
 
     Random rand = new Random();
     int randomElement = availableColumn.get(rand.nextInt(availableColumn.size()));
@@ -52,7 +49,7 @@ public class iwanttheconnectfourprize extends Player {
 //      if
 //    }
 
-    int bestMove = 4;
+    int bestMove = -1;
     int bestValue = Integer.MIN_VALUE;
     for(int i = 0; i < 10 ; i ++) {
       try{
@@ -63,14 +60,15 @@ public class iwanttheconnectfourprize extends Player {
           bestValue = moveValue;
         }
       } catch (InvalidMoveException e) {
+        //deliberately not doing anything, but probably not good
       }
-      return bestMove;
     }
 
+    //this is a fallback, comment out in testing, but can add in real game
+    //if(bestMove == -1) return randomElement;
 
+    return bestMove;
 
-
-    return randomElement;
   }
 
   public int minimax(Board board, int depth, boolean isMaximisingPlayer, int alpha, int beta) {
@@ -79,14 +77,81 @@ public class iwanttheconnectfourprize extends Player {
       int maxEval = Integer.MIN_VALUE;
       for(int i = 0; i < 10; i++){
         try{
+            Board newBoard = new Board(board, i, this.getCounter());
+            int eval = minimax(newBoard, depth +1, false, alpha, beta);
+            maxEval = Math.max(maxEval,eval);
+            if(beta <= alpha) break;
+        } catch (InvalidMoveException e) {
+        }
+      }
+      return maxEval;
+    } else {
+      int minEval = Integer.MAX_VALUE;
+      for(int i = 0; i < 10; i ++) {
+        try {
+          Counter opponentCounter = this.getCounter().getOther();
+          Board newBoard = new Board(board, i, opponentCounter);
+          int eval = minimax(newBoard, depth+1, true, alpha, beta);
+          minEval = Math.min(minEval, eval);
+          beta = Math.min(beta,eval);
+          if(beta <= alpha) break;
+        } catch (InvalidMoveException e) {
+          //probably do something here, if have time
+        }
+      }
+      return minEval;
+    }
+  }
 
+
+  public int evaluateBoard(Board board, Counter myCounter){
+    int score = 0;
+    Counter opponentCounter = myCounter.getOther();
+
+    for(int i = 0; i < 10; i++) {
+      for(int j = 0; j < 8; j ++){
+        Position currentPosition = new Position(i,j);
+        if(board.isWithinBoard(currentPosition)) {
+          Counter currentCounter = board.getCounterAtPosition(currentPosition);
+
+          //is there way to modify this? it checks horizontal, vertical and diagonal each time
+          score += evaluateLine(board, currentPosition, 1,0, myCounter,opponentCounter);
+          score += evaluateLine(board, currentPosition, 0,1, myCounter,opponentCounter);
+          score += evaluateLine(board, currentPosition, 1,1, myCounter,opponentCounter);
+          score += evaluateLine(board, currentPosition, 1,-1, myCounter,opponentCounter);
         }
       }
     }
-    return 0;
-  }
-
-  public int evaluateBoard(Board board, Counter myCounter){
-    return 0;
+    return score;
   };
+
+  //ask the faculty if there's a way to improve, seems like inefficient and may not be a good evaluation function
+  public int evaluateLine(Board board, Position start, int dx, int dy, Counter myCounter, Counter opponentCounter){
+    int myCount = 0;
+    int opponentCount = 0;
+    int score = 0;
+
+    for (int i = 0; i < 4; i++){
+      int x = start.getX() + i*dx;
+      int y = start.getY() + i*dy;
+      Position currentPosition = new Position(x, y);
+
+      if(board.isWithinBoard(currentPosition)) {
+        Counter currentCounter = board.getCounterAtPosition(currentPosition);
+        if(currentCounter == myCounter){
+          myCount ++;
+        } else if (currentCounter == opponentCounter) {
+          opponentCount ++;
+        }
+      }
+    }
+
+    if(myCount == 4) score += 1000;
+    else if (myCount == 3 && opponentCount == 0) score += 50;
+    else if (myCount == 2 && opponentCount == 0) score += 10;
+
+    if(opponentCount == 4) score -= 1000;
+
+    return score;
+  }
 }
